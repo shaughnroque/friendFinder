bc834f11bff916896510e7adbc3abceb9de2af71
Shaughn Roque <shaughnroque@Shaughns-MacBook-Pro.local>
2018-04-25 22:22:24 -0700

    update 3

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ connection.connect();
 moduel.exports = connection;
 
 var app = express();
-var PORT = 8080;
+var PORT = process.env.PORT || 8080;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
