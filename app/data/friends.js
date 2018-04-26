ccbf7871931f4ef915e6d49feeb74f124db357f1
Shaughn Roque <shaughnroque@Shaughns-MacBook-Pro.local>
2018-04-25 22:02:05 -0700

    update

---
+++ app/data/friends.js
@@ -0,0 +1,79 @@
+var friends = [{
+    name: "Mike",
+    photo: "https://naqyr37xcg93tizq734pqsx1-wpengine.netdna-ssl.com/wp-content/uploads/2016/01/75-Motivational-Michael-Jordan-Quotes.jpg",
+    score: [
+        3,
+        1,
+        5,
+        2,
+        1,
+        1,
+        5,
+        5,
+        5,
+        5
+    ]
+},
+{
+    name: "Charles",
+    photo: "https://usatftw.files.wordpress.com/2016/07/c03_barkley_before_13.jpg?crop=0px%2C121px%2C2302px%2C1381px&resize=1000%2C600",
+    score: [
+        5,
+        1,
+        2,
+        2,
+        2,
+        3,
+        4,
+        5,
+        3,
+        2
+    ]
+},
+{
+    name: "Shaq",
+    photo: "https://tombuchheim.files.wordpress.com/2011/06/shaq.jpg",
+    score: [
+        1,
+        2,
+        3,
+        4,
+        5,
+        5,
+        3,
+        2,
+        2,
+        1
+    ],
+    name: "Kobe",
+    photo: "https://pmcdeadline2.files.wordpress.com/2017/12/121207122711-kobe-bryant-smile-120712_1280x720.jpg?w=446&h=299&crop=1",
+    score: [
+        5,
+        5,
+        5,
+        5,
+        5,
+        2,
+        2,
+        1,
+        1,
+        1
+    ],
+    name: "Lebron",
+    photo: "https://www.gannett-cdn.com/-mm-/7a088501706639aa7234ea47a99c0589b5e41ed5/c=62-48-1190-1552&r=537&c=0-0-534-712/local/-/media/2018/02/02/USATODAY/USATODAY/636531733602097146-USP-NBA--Cleveland-Cavaliers-at-Detroit-Pistons.jpg",
+    score: [
+        1,
+        1,
+        1,
+        1,
+        3,
+        3,
+        4,
+        2,
+        2,
+        1
+    ],
+}
+];
+
+module.exports = friends;
