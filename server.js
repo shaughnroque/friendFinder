0e79572e6393167c20728d32ff839d2c88337f88
Shaughn Roque <shaughnroque@Shaughns-MacBook-Pro.local>
2018-04-25 22:33:39 -0700

    final update

--- server.js
+++ server.js
@@ -1,23 +1,6 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var path = require("path");
-var mysql = require("mysql");
-var connection;
-
-if (process.env.JAWSDB_URL) {
-    connection = mysql.createConnection(process.env.JAWSDB_URL);
-
-} else {
-    connection = mysql.createConnection({
-        host: "localhost",
-        user: "root",
-        password: "hacktheplanet",
-        database: "todoagain_db"
-    });
-};
-
-connection.connect();
-moduel.exports = connection;
 
 var app = express();
 var PORT = process.env.PORT || 8080;
